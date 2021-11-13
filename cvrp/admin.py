8d7739867940b6f10e63205a65f8e13557a2d437
namelesshero <ragung669@gmail.com>
2021-11-13 22:09:05 +0700

    chore: update django admin

--- cvrp/admin.py
+++ cvrp/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
-from .models import Depot, Courier, Client
+from django import forms
+from .models import *
 
 @admin.register(Depot)
 class DepotAdmin(LeafletGeoAdmin):
     list_display = ['name', 'address']
+    exclude = ('user',)
+    
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
 
     def get_queryset(self, request):
         qs = super(DepotAdmin, self).get_queryset(request)
@@ -15,6 +21,11 @@ class DepotAdmin(LeafletGeoAdmin):
 @admin.register(Courier)
 class CourierAdmin(LeafletGeoAdmin):
     list_display = ['name', 'capacity']
+    exclude = ('user',)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
 
     def get_queryset(self, request):
         qs = super(CourierAdmin, self).get_queryset(request)
@@ -25,20 +36,14 @@ class CourierAdmin(LeafletGeoAdmin):
 @admin.register(Client)
 class ClientAdmin(LeafletGeoAdmin):
     list_display = ['name', 'address', 'demand']
+    exclude = ('user',)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
 
     def get_queryset(self, request):
         qs = super(ClientAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qs
         return qs.filter(user = request.user.id)
-
-# class ClientAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Client
-#         fields = "__all__"
-
-
-#     def __init__(self, request, *args, **kwargs):
-#         super(ClientAdminForm, self).__init__(request, *args, **kwargs)
-#         if self.instance:
-#             self.fields['user'].queryset = request.user
