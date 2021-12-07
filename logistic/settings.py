48f690a2f2f5b304c37c922e88ce2ff891fdc0bf
namelesshero <ragung669@gmail.com>
2021-12-07 23:21:31 +0700

    try to fix staticfiles

--- logistic/settings.py
+++ logistic/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 import base64
 import dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 # dotenv_path = Path('../.env')
 load_dotenv()
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = base64.b64decode(os.environ.get('SECRET_KEY')).decode('ascii')
+SECRET_KEY = base64.b64decode(os.environ.get('SECRET_KEY')).decode('ascii') or get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG')
@@ -50,7 +51,8 @@ INSTALLED_APPS = [
     'leaflet',
     'rest_framework',
     'rest_framework_gis',
-    'users'
+    'users',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -143,9 +145,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
