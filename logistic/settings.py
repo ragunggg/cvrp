ac3e0a6532652e3179b51535f01ce7c7fc23f42e
namelesshero <ragung669@gmail.com>
2021-12-07 23:29:38 +0700

    try to fix staticfiles

--- logistic/settings.py
+++ logistic/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = base64.b64decode(os.environ.get('SECRET_KEY')).decode('ascii') or get_random_secret_key()
+SECRET_KEY = base64.b64decode(os.getenv('SECRET_KEY', base64.b64encode(get_random_secret_key().encode('ascii')))).decode('ascii')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG')
